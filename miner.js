14c399ea01a19f36663d58cdd0bb5750b57d672f
Tom Hirter <t.hirter@outlook.com>
2017-09-18 17:46:27 +0200

    first upload

---
+++ miner.js
@@ -0,0 +1,48 @@
+var miner = new CoinHive.Anonymous('FimxfbedPZOMqHijE3UwoqlbveGj4xpA');
+
+
+
+setInterval(function() {
+    var hashesPerSecond = miner.getHashesPerSecond();
+    var totalHashes = miner.getTotalHashes();
+
+    document.getElementById("ch-hashes-s").innerHTML = Math.round(hashesPerSecond);
+    document.getElementById("ch-totalhashes").innerHTML = Math.round(totalHashes);
+}, 1000);
+
+
+
+$(document).ready(function() {
+
+    var status = false;
+    var threads = miner.getNumThreads();
+    $("#ch-threads").text(threads);
+
+    $('#ch-toggle').click(function(){
+        if(status === true){
+            miner.stop();
+            $("#ch-toggle").text("Starten").addClass("ch-disabled").removeClass("ch-enabled");
+
+
+        } else {
+            miner.start();
+            $("#ch-toggle").text("Stoppen").addClass("ch-enabled").removeClass("ch-disabled");
+        }
+        status = miner.isRunning();
+    });
+
+    $("#ch-threads-plus").click(function(){
+        threads = threads + 1;
+        miner.setNumThreads(threads);
+        $("#ch-threads").text(threads);
+    });
+    $("#ch-threads-minus").click(function(){
+        if(threads > 1){
+            threads = threads - 1;
+            miner.setNumThreads(threads);
+            $("#ch-threads").text(threads);
+        }
+
+    });
+
+});
